82fda4953c07cfab8f103a48386fe36b518ae5de
Aaron Hoffman <aaronhoffman@Lilly-Pad-Mac.local>
2022-01-30 22:42:49 -0800

    added alb listener rules

--- main.tf
+++ main.tf
@@ -31,14 +31,19 @@ resource "aws_security_group" "instance" {
 }
 resource "aws_autoscaling_group" "example" {
   launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier = data.aws_subnet_ids.default.ids
+  
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
+  
   min_size = 2
   max_size = 10
+  
   tag {
     key = "Name"
     value = "terraform-asg-example"
     propagate_at_launch = true
   }
-  vpc_zone_identifier = data.aws_subnet_ids.default.ids
 }
 variable "server_port" {
   description = "traffic port"
@@ -49,6 +54,7 @@ resource "aws_lb" "example" {
   name = "terraform-asg-example"
   load_balancer_type = "application"
   subnets = data.aws_subnet_ids.default.ids
+  security_groups = [aws_security_group.alb.id]
 }
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.example.arn
@@ -62,4 +68,51 @@ resource "aws_lb_listener" "http" {
       status_code = 404
     }
   }  
+}
+resource "aws_security_group" "alb" {
+  name = "terraform-example-alb"
+  ingress = {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress = {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+resource "aws_lb_target_group" "asg" {
+  name = "terraform-asg-example"
+  port = var.server_port
+  protocol = "HTTP"
+  vpc_id = data.aws_vpc.default.id
+  health_check {
+    path = "/"
+    protocol = "HTTP"
+    matcher = "200"
+    interval = 15
+    timeout = 3
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+  }
+}
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority = 100
+  condition {
+    path_pattern {
+      values = [*]
+    }
+  }
+  action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
+}
+output "alb_dns_name" {
+  value = aws_lb.example.dns_name
+  description = "Load balancer domain name"
 }
