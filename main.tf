f82bfc0657da2721382b07bff8610b0a7e031486
Aaron Hoffman <aaronhoffman@Lilly-Pad-Mac.local>
2022-01-30 22:15:56 -0800

    fixed typo in listener

--- main.tf
+++ main.tf
@@ -51,7 +51,7 @@ resource "aws_lb" "example" {
   subnets = data.aws_subnet_ids.default.ids
 }
 resource "aws_lb_listener" "http" {
-  loadload_balancer_arn = aws_lb.example.certificate_arn
+  load_balancer_arn = aws_lb.example.arn
   port = 80
   protocol = "HTTP"
   default_action {
